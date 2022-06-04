f57d80df34b3e7ff0cb0be21d591271dd57a97fa
calistus-igwilo <ebunilo@yahoo.com>
2022-06-04 17:01:19 +0100

    add docstring

--- baseball-game.py
+++ baseball-game.py
@@ -1,37 +1,41 @@
 # Baseball Game
-"""
-You are keeping score for a baseball game with strange rules. 
-The game consists of several rounds, where the scores of past rounds 
-may affect future rounds' scores.
 
-At the beginning of the game, you start with an empty record. 
-You are given a list of strings ops, where ops[i] is the ith operation 
-you must apply to the record and is one of the following:
 
-1. An integer x - Record a new score of x.
+def baseball_game(ops):
+    '''
+    You are keeping score for a baseball game with strange rules. 
+    The game consists of several rounds, where the scores of past rounds 
+    may affect future rounds' scores.
 
-2. "+" - Record a new score that is the sum of the previous two scores. 
-It is guaranteed there will always be two previous scores.
+    At the beginning of the game, you start with an empty record. 
+    You are given a list of strings ops, where ops[i] is the ith operation 
+    you must apply to the record and is one of the following:
 
-3. "D" - Record a new score that is double the previous score. 
-It is guaranteed there will always be a previous score.
+    An integer x - Record a new score of x.
 
-4. "C" - Invalidate the previous score, removing it from the record. 
-It is guaranteed there will always be a previous score.
+    "+" - Record a new score that is the sum of the previous two scores. 
+    It is guaranteed there will always be two previous scores.
 
-"""
+    "D" - Record a new score that is double the previous score. 
+    It is guaranteed there will always be a previous score.
 
-ops = ["5", "2", "C", "D", "+"]
-result = []
-for n in ops:
-    if n == "+":
-        result.append(result[-1] + result[-2])
-    elif n == "D":
-        result.append(2 * result[-1])
-    elif n == "C":
-        result.pop()
-    else:
-        result.append(int(n))
-print(sum(result))
+    "C" - Invalidate the previous score, removing it from the record. 
+    It is guaranteed there will always be a previous score.
+
+    '''
+    #ops = ["5", "2", "C", "D", "+"]
+    result = []
+    for n in ops:
+        if n == "+":
+            result.append(result[-1] + result[-2])
+        elif n == "D":
+            result.append(2 * result[-1])
+        elif n == "C":
+            result.pop()
+        else:
+            result.append(int(n))
+    return sum(result)
+
+print(baseball_game(["5", "2", "C", "D", "+"]))
 
 
