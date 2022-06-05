2fb3890c581aca6e093ece3399ece699d1e552e0
calistus-igwilo <ebunilo@yahoo.com>
2022-06-05 13:32:02 +0100

    slicing done

--- slicing.py
+++ slicing.py
@@ -8,3 +8,18 @@ filename = 'veiw.jpg'
 print(filename[-3:])
 # or
 print(filename[5:])
+
+# remove spaces
+"""
+From the following text
+    string = '1 0 0 1 0 1'
+remove spaces using slicing. Then convert the result to 
+decimal notation and print to the console as shown below:
+
+Number found: 37
+"""
+
+string = '1 0 0 1 0 1'
+binary = string[::2]
+number = int(binary, 2)
+print(f'Number found: {number}')
