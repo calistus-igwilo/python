e19d89faf40d5e6621164089cf1bb27ee0283f13
calistus-igwilo <ebunilo@yahoo.com>
2022-06-05 10:39:49 +0100

    add fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+def fizzBuzz(n):
+    '''
+    Given a number 
+    '''
+    for i in range(1, n+1):
+        if i//3 = 0 and n//5 = 0:
+            print(FizzBuZZ)
+        elif i//3 = 0 and i//5 != 0:
+            print(Fizz)
+        elif i//3 != 0 and i//5 = 0:
+            print(Buzz)
+        else:
+            print(i)
--- sandbox.py
+++ sandbox.py
@@ -1,4 +1,4 @@
 first, *last = [1, 2, 3, 4, 5, 6, 7]
 print("first: ", first)
-print("second: ", second)
-print("third: ", last)
+#print("second: ", second)
+print("last: ", last)
