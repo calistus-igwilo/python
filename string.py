43d4ff1603ccde7dc8050fd7bf63c959be42de26
calistus-igwilo <ebunilo@yahoo.com>
2022-06-26 10:32:53 +0100

    Palindrome methods 1 to 3

--- string.py
+++ string.py
@@ -64,6 +64,54 @@ def non_repeat(s):
 print(non_repeat(s))
 
 
+"""
+You are given a string. Write a function to check whether the string is a 
+palindrome or not.
+"""
+def palindrome(s):
+    str_ = ""
+    for i in range(len(s)-1, -1, -1):
+        str_ += s[i]
+    print(str_)
+    if str_ == s:
+        return True
+    return False
+    # Time complexity = O(n**2) because strings are immutable, so append creates a
+    # new string for each call.
+    # Space complexity = O(n)
+s = "abba"
+print(palindrome(s))
+
+# Method 2: Appending to an array
+def palindrome(s):
+    arr = []
+    for i in range(len(s)-1, -1, -1):
+        arr.append(s[i])
+    result = ''.join(arr)
+    if s == result:
+        return True
+    return False
+    # Time complexity = O(n) because append to array is a constant time operation
+    # and the array was traversed once.
+    # Space complexity = O(n) as extra sapce was created to store the new array
+print(palindrome(s))
+
+# Method 3
+def palindrome(s):
+    i = 0
+    j = len(s)-1
+    while i <= j:
+        if s[i] != s[j]:
+            return False
+        i += 1
+        j -= 1
+    return True
+    # Time complexity = O(n)
+    # Space complexity = O(1) since no extra space was created
+print(palindrome(s))
+
+
+
 
 
 
