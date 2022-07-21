9c37d37e5e1e375fc331053da7be7492df5baa2b
calistus-igwilo <ebunilo@yahoo.com>
2022-07-21 19:50:13 +0100

    Binary search tree

--- trees.py
+++ trees.py
@@ -5,3 +5,25 @@ Design a Binary Search Tree class that supports the following:
 3. Find a value. If the value is found it should return the node with the value
     else return false.
 """
+from turtle import left
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        node = Node()
+        if not self.root:
+            self.root = node 
+            return self
+
+    
+
