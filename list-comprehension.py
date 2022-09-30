b7955b01ca68dd9158436af565397e38b3c91b00
calistus-igwilo <ebunilo@yahoo.com>
2022-09-30 03:13:31 +0100

    cordinates of a cuboid on 3D grid

--- list-comprehension.py
+++ list-comprehension.py
@@ -144,3 +144,12 @@ data = dict(zip(('a', 'b', 'c', 'd', 'e', 'f'),(1, 2, 3, 4, 5, 6)))
 data = [[key, val] for key, val in data.items()]
 print(data)
 
+
+"""
+You are given three integers x, y and z representing the dimensions of a cuboid along with an 
+integer n. Print a list of all possible coordinates given by (i,j,k) on a 3D grid where 
+the sum of i+j+k is not equal to n.  Here, 0<=i<=x; 0<=j<=y<=k<=z. Use list comprehensions
+"""
+x=1; y=1; z=2; n=3
+result = [[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if (i+j+k) != n]
+print(result) 
