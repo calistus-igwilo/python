6d8408af2cde75ec8332f80281cf2ef011f98ee0
calistus-igwilo <ebunilo@yahoo.com>
2022-10-01 13:24:18 +0100

    The minion game: substrings in a string

--- string.py
+++ string.py
@@ -249,6 +249,40 @@ def compress_string(S):
     print(result)
 
 
+"""
+Kevin and Stuart want to play the 'The Minion Game'.
+
+Game Rules
+
+Both players are given the same string, .
+Both players have to make substrings using the letters of the string .
+Stuart has to make words starting with consonants.
+Kevin has to make words starting with vowels.
+The game ends when both players have made all possible substrings.
+
+Scoring
+A player gets +1 point for each occurrence of the substring in the string .
+"""
+def minion_game(string_):
+    vowel_strings = 0
+    consonant_strings = 0
+    string_length = len(string_) # length of string
+
+    for i in range(string_length):
+        if string_[i] in "AEIOU":
+            vowel_strings += string_length - i  # the number of substrings that can be formed from the length
+        else:
+            consonant_strings += string_length -i # the number of substrings that can be formed
+
+    # Determine the winner
+    if consonant_strings > vowel_strings:
+        print("Stuart", consonant_strings )
+    elif consonant_strings < vowel_strings:
+        print("Kevin", vowel_strings)
+    else:
+        print("Draw")
+
+minion_game("BANANA")
 
 
 
